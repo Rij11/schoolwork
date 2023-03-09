018f6d6880321099e7e77919756e6fe040309e31
Rij11 <rij11.real@gmail.com>
2023-03-09 22:25:30 +1000

    Added Dig Sol Draft

--- Digital Solutions/Week 6-8 Draft/Activity 5/Wk6 FIA1 Draft RoboAnt 5 V1.js
+++ Digital Solutions/Week 6-8 Draft/Activity 5/Wk6 FIA1 Draft RoboAnt 5 V1.js
@@ -1,29 +1,18 @@
-//Wk6 FIA1 Draft V1.3
-
+//Wk6 FIA1 Draft V1.8
 
 //Increase Movement Speed
 Change_Delay_To(100);
 
 
 //Variables
-
 var markers = 0;    //Counts Markers
 var stop = 0;   //Stops RoboAnt from picking up markers he drops at the end
 var moves = 0;	//Counts every Move
 var turns = 0;  //Counts every turn (1 For Right & left, & 2 for turning around)
 
 
-//  Functions
-
-//Turn Right
-function turnRight() {
-    Turn_Left();
-    Turn_Left();
-    Turn_Left();
-}
-
-//Explore maze function
-function gogoMaze() {
+//Explore maze loop process
+while (stop == 0) {
     if (On_A_Marker() && stop==0) {   // Picks Up Marker
         Pick_Up_Marker();
         markers++;
@@ -37,38 +26,25 @@ function gogoMaze() {
 		while (Any_Markers_In_Marker_Bag())	{   //Drops all markers
 			Put_Down_Marker();
 		}
+		Say('Coins / Turns: ' + markers/turns*100 + '%');
+        Say('Turns / Length: ' + turns/moves*100 + '%');
 	}
     else if (Front_Is_Blocked() && Right_Is_Blocked() && Left_Is_Blocked()) { //Turn around at end
         Turn_Left();
         Turn_Left();
-        Move();
-        moves++;
         turns=turns+2;
     }
     else if (Front_Is_Blocked() && Left_Is_Blocked()) { //Turns right
-        turnRight();
-        Move();
-        moves++;
+        for (var x=1; x<=3; x++) {
+            Turn_Left();
+        }
         turns++;
     }
     else if (Front_Is_Blocked() && Right_Is_Blocked()) {  //Turn Left
         Turn_Left();
-        Move();
-        moves++;
         turns++;
     }
 }
-
-
-//  Sequence
-
-for (var x=1; x<=50; x++) { //Runs Maze function few times ~~Can't loop actual function for some reason
-gogoMaze();
-}
-
-//Say data
-Say('Coins / Turns: ' + markers/turns*100 + '%');
-Say('Turns / Length: ' + turns/moves*100 + '%');
 
 
 //END
