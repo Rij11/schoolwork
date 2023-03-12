62a510c7163cdf4130e106505680d251d7465029
Rij11 <rij11.real@gmail.com>
2023-03-12 13:28:07 +1000

    Updated RoboAnt Task 6

--- Digital Solutions/Week 6-8 Draft/Wk7 RoboAnt 6.js
+++ Digital Solutions/Week 6-8 Draft/Wk7 RoboAnt 6.js
@@ -1,53 +1,20 @@
-//RoboAnt 6 V1.0
+//RoboAnt 6 V2.0
 
+Change_Delay_To(100);   //Increase Movement Speed
 
-//Increase Movement Speed
-Change_Delay_To(100);
-
-
-//	Variables
-
-var stop = 0;	//Used to top movement after picking uo a marker
-
-
-//  Functions
-
-//Turn Right
-function turnRight() {
-    Turn_Left();
-    Turn_Left();
-    Turn_Left();
-}
-
-//Hill Climb function
-function climbHill() {
-    if (On_A_Marker() && stop==0) {   // Picks Up Marker
-        Pick_Up_Marker();
-    }
-	else if (Any_Markers_In_Marker_Bag() && stop==0) {
-		stop++;
-	}
-	else if (Right_Is_Clear() && stop==0) {	//Turn at end of pillar
-		turnRight();
+while (Not_On_A_Marker()) {
+	if (Right_Is_Clear()) {	//Turn at end of pillar
+		Turn_Left();
+		Turn_Left();
+		Turn_Left();
 		Move();
-	}
-    else if (Front_Is_Clear() && stop==0){  //Moves forwards
+	} else if (Front_Is_Clear()){  //Moves forwards
         Move();
-    }
-	else if (Front_Is_Blocked() && Right_Is_Blocked() && stop==0) {  //Turn Left
+    } else if (Left_Is_Clear()) {  //Turn Left
+        Turn_Left();
+    } else if (Front_Is_Blocked()) { //Turns around
+        Turn_Left();
         Turn_Left();
     }
-    else if (Front_Is_Blocked() && Left_Is_Blocked() && stop==0) { //Turns right
-        turnRight();
-    }
 }
-
-
-//  Sequence
-
-for (var x=1; x<50; x++) { //Runs climb function few times ~~Can't loop actual function for some reason
-climbHill();
-}
-
-
-//END
+Pick_Up_Marker();
